c00ab1586ce907f33c842eecdd1716d4f1385d42
sisshmue <sisshmue404@gmail.com>
2026-03-30 16:27:48 +0700

    add soft delete and other CRUD functions

--- src/respositories/asset.repository.js
+++ src/respositories/asset.repository.js
@@ -1,9 +1,42 @@
 import prisma from "../configs/prisma.js";
 
-export const createAssets = (data)=>{
-    return prisma.asset.create({data})
-}
+export const createAssets = (data) => {
+  return prisma.asset.create({ data });
+};
 
-export const getAllAssets = ()=>{
-    return prisma.asset.findMany();
-}
+export const getAllAssets = () => {
+  return prisma.asset.findMany({
+    where: { isDeleted: false },
+  });
+};
+
+export const findAsset = (id) => {
+  return prisma.asset.findUnique({
+    where: { id },
+  });
+};
+
+export const updateAsset = (id, data) => {
+  return prisma.asset.update({
+    data,
+    where: { id },
+  });
+};
+
+export const deleteAsset = (id) => {
+  return prisma.asset.update({
+    data: {
+      isDeleted: true,
+    },
+    where: { id },
+  });
+};
+
+export const restoreAsset = (id) => {
+  return prisma.asset.update({
+    data: {
+      isDeleted: false,
+    },
+    where: { id },
+  });
+};
